1505c4c0c28a57118b524539cf9ac3cf49bfcec1
Jmondonedor <jmr3811305@gmail.com>
2023-11-20 22:07:37 -0500

    Animaciones agregadas
    Animaciones a los diversos elementos del index para cuando entran y salen de la pantalla.

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ function abrirLogin(){
 function cerrarLogin(){
     document.getElementById("loginOverlay").style.display="none";
 }
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting) {
+            entry.target.classList.add('mostrar');
+        }else{
+            entry.target.classList.remove('mostrar');
+        }
+    })
+})
+
+const hiddenList=document.querySelectorAll('.hide');
+hiddenList.forEach((e)=>observer.observe(e));
