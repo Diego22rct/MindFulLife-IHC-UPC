6c8b45b96cd2b12697688e23f6d135d5f474e24d
Jmondonedor <96555993+Jmondonedor@users.noreply.github.com>
2023-11-23 07:39:58 -0500

    .

--- loginScript.js
+++ loginScript.js
@@ -1,7 +1,14 @@
 const container = document.getElementById('container-overlay');
 const registerBtn = document.getElementById('registro');
 const loginBtn = document.getElementById('login');
-const closeBtn = document.getElementById('closeBtn')
+const closeBtn = document.getElementById('closeBtn');
+
+const nombreRegistro=document.getElementById("nombreRegister");
+const emailRegistro=document.getElementById("emailRegister");
+const passwordRegistro=document.getElementById("passwordRegister");
+
+const emailLogin=document.getElementById("emailLogin");
+const passwordLogin=document.getElementById("passwordLogin");
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("activo");
@@ -13,3 +20,11 @@ loginBtn.addEventListener('click', () => {
     closeBtn.style.color="white";
 });
 
+function ValidarNombre(){
+    var valorTexto=nombreRegistro.value;
+    return (valorTexto.length>=1 && valorTexto.length<=20)?true:false;
+}
+function ValidarEmail(){
+    var valorTexto=emailRegistro.value;
+    return (valorTexto.length>=1 && valorTexto.length<=20)?true:false;
+}
